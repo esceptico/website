4adbf6da6920da4975bbd9f75747d012bec8a3a1
Timur Ganiev <t.ganiev@replika.ai>
2025-12-13 07:14:34 +0400

    add redirect for old urls (docs -> log)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/docs',
+        destination: '/log',
+        permanent: true,
+      },
+      {
+        source: '/docs/:slug',
+        destination: '/log/:slug',
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config) => {
     // Add support for WebAssembly
     config.experiments = {
